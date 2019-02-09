105b252deda0820993a804cd81d6e2774ce24e4c
gjrett <gjrett@yahoo.com>
2019-02-09 15:26:35 -0600

    Added index.php and img folder with my photo

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
+        <style>
+            body
+            {text-align: center;
+             padding-bottom: 50px;
+            }
+            .center {
+                margin: auto;
+                width: 50%;
+                border: 3px solid blue;
+                padding: 10px;
+            }
+        </style>
+        <h1>Greg Retter</h1>
+        <title>Greg's Website!</title>
+    </head>
+    <body>
+        
+        <img alt = "me" src = "img\me.jpg" />
+
+        <h2>About Me</h2>
+
+        <div class = center><p>I'm a professional Electrical Engineer specializing in controls and automation projects for over 27 years.  I've done a variety of programming and found that the programming and integration is my favorite part of the projects I've worked on.  So now I'm working toward a Master's in Software Engineering and plan to get into more data aquisition and Big Data type project work.
+        </p></div>
+
+        <h2>Random Quote</h2>
+
+   </body>
+</html>
+
+
+  
