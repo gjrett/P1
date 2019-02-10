c12d5474e015c3fea720c99f63c1f466e282a292
gjrett <gjrett@yahoo.com>
2019-02-10 13:01:02 -0600

    Added quotes.php and updated index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require('quotes.php'); ?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -12,6 +13,7 @@
                 width: 50%;
                 border: 3px solid blue;
                 padding: 10px;
+                font-size: 20px;
             }
         </style>
         <h1>Greg Retter</h1>
@@ -23,10 +25,12 @@
 
         <h2>About Me</h2>
 
-        <div class = center><p>I'm a professional Electrical Engineer specializing in controls and automation projects for over 27 years.  I've done a variety of programming and found that the programming and integration is my favorite part of the projects I've worked on.  So now I'm working toward a Master's in Software Engineering and plan to get into more data aquisition and Big Data type project work.
+        <div class = center><p>I'm a professional Electrical Engineer specializing in controls and automation projects for over 27 years.  I've done many types of projects and found that the programming and integration is my favorite part.  So now I'm working toward a Master's in Software Engineering to become more versed in programming and I plan to get into more data aquisition and Big Data type project work.  Toward that end, my company has just completed a Hadoop server cluster and I've joined the Big Data consortium that is developing the data projects.
         </p></div>
 
         <h2>Random Quote</h2>
+
+        <div class = center><p><?php echo $quote; ?></p></div>
 
    </body>
 </html>
---
+++ quotes.php
@@ -0,0 +1,18 @@
+<?php
+
+$quotes = [
+    0 => 'Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world. - Albert Einstein',
+    1 => 'Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid. - Albert Einstein',
+    2 => 'Its like deja vu all over again. - Yogi Berra',
+    3 => 'Education is what remains after one has forgotten what one has learned in school. - Albert Einstein',
+    4 => 'I can resist everything except temptation. - Oscar Wilde',
+    5 => 'The important thing is to not stop questioning. Curiosity has its own reason for existing. - Albert Einstein',
+    6 => 'Concern for man and his fate must always form the chief interest of all technical endeavors. Never forget this in the midst of your diagrams and equations. - Albert Einstein',
+    7 => 'There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle - Albert Einstein',
+    8 => 'The highest result of education is tolerance. - Helen Keller',
+    9 => 'I have never let my schooling interfere with my education. - Mark Twain'
+
+];
+
+$quote = $quotes[rand(0,9)];
+
